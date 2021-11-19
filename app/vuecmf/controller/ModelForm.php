121cbd3941719040222366ec5b099dee5e28505c
emei8 <2278667823@qq.com>
2021-11-19 19:36:23 +0800

    完成模型表单及模型规则的表设计

---
+++ app/vuecmf/controller/ModelForm.php
@@ -0,0 +1,55 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020~2021 http://www.vuecmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( https://github.com/emei8/vuecmf/blob/master/LICENSE )
+// +----------------------------------------------------------------------
+// | Author: emei8 <2278667823@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
+
+namespace app\vuecmf\controller;
+
+use think\Request;
+
+/**
+ * 模型表单管理
+ * Class ModelAction
+ * @package app\vuecmf\controller
+ */
+class ModelForm extends Base
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
---
+++ app/vuecmf/model/ModelAction.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020~2021 http://www.vuecmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( https://github.com/emei8/vuecmf/blob/master/LICENSE )
+// +----------------------------------------------------------------------
+// | Author: emei8 <2278667823@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
+
+namespace app\vuecmf\model;
+
+use think\Model;
+
+/**
+ * @mixin \think\Model
+ */
+class ModelAction extends Model
+{
+    //
+}
